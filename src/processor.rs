4e262f171b8e0e625d4c2c2bb208b42ecb549a8d
Vijay <vijay@iavian.com>
2025-08-13 16:31:12 +0530

    alright

--- src/processor.rs
+++ src/processor.rs
@@ -29,13 +29,14 @@ impl ApnsProcessor {
             device_hash_filter = format!("dm.devicehash = '{device_hash}' AND");
         }
         let sql = format!(
-            "SELECT nm.url, dm.id, nm.id AS nId, dm.devicehash, dm.token, dm.sound_id, trim(nm.text) AS text, \
+            "SELECT CASE WHEN LENGTH(COALESCE(nm.url, '')) < 3 THEN CONCAT('https://breaking.iavian.net/article/', nm.id)::TEXT ELSE url END AS url, dm.id, nm.id AS nId, dm.devicehash, dm.token, dm.sound_id, trim(nm.text) AS text, \
             (case when _from AT TIME ZONE 'UTC' < _to AT TIME ZONE 'UTC' then ((_from, _to) OVERLAPS (current_time, current_time)) \
             else (case when _from <= current_time OR _to >= current_time then true else false end) end) AS playsound, \
             dm.paid, extract(epoch from nm.news_date)::BIGINT AS news_date, dm.type, nm.news_id \
             FROM apns_master dm, news_master nm \
             WHERE (dm.news_id & nm.news_id = nm.news_id) AND {device_hash_filter} nm.id = $1"
         );
+
         let pool = Arc::new(pool);
         let stream = sqlx::query_as::<_, PushNotification>(&sql).bind(news_id).fetch(&*pool);
         let concurrency_limit = self.max_concurrent_requests * (self.clients.len() / 2);
