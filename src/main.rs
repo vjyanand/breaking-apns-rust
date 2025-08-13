674f8ab4ebbdea7a1a69e3fc5fce4bf636b3df6d
Vijay <vijay@iavian.com>
2025-08-13 16:49:21 +0530

    jmellaoc

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use std::env;
 
 use actix_web::middleware::{self, Logger};
 use actix_web::{App, HttpServer};
+use jemallocator::Jemalloc;
 use log::warn;
 
 use crate::handler::ok;
 use crate::handler::push;
+
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn"))
