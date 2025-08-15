8e09a350d14f7057b9e6443704341f8efe2af9dc
Vijay <vijay@iavian.com>
2025-08-15 19:47:31 +0530

    d

--- src/client.rs
+++ src/client.rs
@@ -87,8 +87,10 @@ impl ApnsClient {
             let apns_id = Uuid::parse_str(&apns_id_header);
             let body_bytes = response.collect().await?.to_bytes();
             let error_message = String::from_utf8_lossy(&body_bytes).to_string();
+            drop(notification);
             return Ok(Some(PushResult { apns_id, success: status.is_success(), status_code: status.as_u16(), error: error_message }));
         }
+        drop(notification);
         Ok(None)
     }
 }
