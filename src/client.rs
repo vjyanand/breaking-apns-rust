8320f0c7ba4e34bffa7f34740edb5db51ab580e0
Vijay <vijay@iavian.com>
2025-09-07 13:33:43 +0530

    f

--- src/client.rs
+++ src/client.rs
@@ -1,28 +1,95 @@
-use std::str::FromStr;
-use std::time::Duration;
+use std::{str::FromStr, time::Duration};
 
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper_tls::HttpsConnector;
-use hyper_util::client::legacy::Client as LegacyClient;
-use hyper_util::{client::legacy::connect::HttpConnector, rt::TokioExecutor};
-
-use crate::config::ApnsConfig;
+use crate::{
+    apns::{PushNotification, PushResult},
+    config::ApnsConfig,
+};
+use http_body_util::{BodyExt, Full};
+use hyper::{Method, Request, Uri, body::Bytes};
+use hyper_rustls::HttpsConnector;
+use hyper_util::{
+    client::legacy::{Client, connect::HttpConnector},
+    rt::{TokioExecutor, TokioTimer},
+};
+use uuid::Uuid;
 
 pub struct ApnsClient {
-    client: LegacyClient<HttpsConnector<HttpConnector>, Full<Bytes>>,
+    client: Client<HttpsConnector<HttpConnector>, Full<Bytes>>,
     base_url: String,
     jwt_token: String,
 }
 
 impl ApnsClient {
     pub fn new(config: &ApnsConfig, jwt_token: &String) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
-        let mut https_connector = HttpsConnector::new();
-        https_connector.https_only(true);
-        let client = LegacyClient::builder(TokioExecutor::new()).http2_only(true).build(https_connector);
         let base_url = if config.sandbox { "https://api.sandbox.push.apple.com" } else { "https://api.push.apple.com" };
+        let https = hyper_rustls::HttpsConnectorBuilder::new().with_native_roots()?.https_only().enable_http2().build();
+        let client: Client<HttpsConnector<HttpConnector>, Full<Bytes>> = Client::builder(TokioExecutor::new())
+            .pool_timer(TokioTimer::new())
+            .pool_idle_timeout(Duration::from_secs(30))
+            .pool_max_idle_per_host(5)
+            .http2_only(true)
+            .http2_initial_stream_window_size(1_048_576) // Max stream window for HTTP/2
+            .http2_initial_connection_window_size(10_485_760) // 1MB connection window
+            .http2_adaptive_window(true)
+            .build(https);
+
         let base_url = String::from_str(base_url)?;
         let jwt_token = String::from(jwt_token);
         Ok(Self { client, base_url, jwt_token })
+    }
+
+    pub async fn send_notification(&self, notification: &PushNotification) -> Result<Option<PushResult>, Box<dyn std::error::Error + Send + Sync>> {
+        let url = format!("{}/3/device/{}", self.base_url, notification.device_token);
+        let uri: Uri = url.parse().unwrap();
+        let payload = serde_json::to_string(&notification.payload).unwrap();
+        let topic = match notification.push_type {
+            crate::apns::BreakingApnsType::Complication => "com.iavian.breakingnews.watchkitapp.complication",
+            crate::apns::BreakingApnsType::App => "com.iavian.breakingnews",
+            crate::apns::BreakingApnsType::Watch => "com.iavian.breakingnews.watchkitapp",
+        };
+        let mut request = Request::builder()
+            .method(Method::POST)
+            .uri(uri)
+            .header("authorization", format!("bearer {}", &self.jwt_token))
+            .header("content-type", "application/json")
+            .header("content-length", payload.len().to_string())
+            .header("apns-topic", topic);
+
+        if let Some(priority) = notification.priority {
+            request = request.header("apns-priority", priority.to_string());
+        }
+
+        if let Some(expiration) = notification.expiration {
+            request = request.header("apns-expiration", expiration.to_string());
+        }
+
+        if let Some(collapse_id) = &notification.collapse_id {
+            request = request.header("apns-collapse-id", collapse_id.as_ref());
+        }
+        if notification.push_type == crate::apns::BreakingApnsType::Complication {
+            request = request.header("apns-push-type", "complication");
+        } else {
+            request = request.header("apns-push-type", "alert");
+        }
+
+        request = request.header("apns-id", &notification.id.to_string());
+
+        let request = request.body(Full::new(Bytes::from(payload)))?;
+        let response = self.client.request(request).await?;
+        let status = response.status();
+
+        if !status.is_success() {
+            let apns_id_header = response
+                .headers()
+                .get("apns-id")
+                .and_then(|v| v.to_str().ok())
+                .map(|s| s.to_string())
+                .unwrap_or(notification.id.to_string());
+            let apns_id = Uuid::parse_str(&apns_id_header);
+            let body_bytes = response.collect().await?.to_bytes();
+            let error_message = String::from_utf8_lossy(&body_bytes).to_string();
+            return Ok(Some(PushResult { apns_id, status_code: status.as_u16(), error: error_message }));
+        }
+        Ok(None)
     }
 }
--- src/processor.rs
+++ src/processor.rs
@@ -2,10 +2,8 @@ use crate::client::ApnsClient;
 use crate::{apns::PushNotification, config::ApnsConfig};
 use futures_util::StreamExt;
 use log::warn;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::{Connection, PgConnection, Pool, Postgres};
+use sqlx::{Pool, Postgres};
 use std::hash::{DefaultHasher, Hash, Hasher};
-use std::time::Duration;
 
 pub struct ApnsProcessor {
     clients: Vec<ApnsClient>,
@@ -21,5 +19,60 @@ impl ApnsProcessor {
             }
         }
         Ok(Self { clients })
+    }
+
+    pub async fn process_notifications(&self, news_id: i64, device_hash: Option<&String>) {
+        let mut device_hash_filter = String::new();
+        if let Some(device_hash) = device_hash {
+            device_hash_filter = format!("dm.devicehash = '{device_hash}' AND");
+        }
+        let sql = format!(
+            "SELECT CASE WHEN LENGTH(COALESCE(nm.url, '')) < 3 THEN CONCAT('https://breaking.iavian.net/article/', nm.id)::TEXT ELSE url END AS url, dm.id, nm.id AS nId, dm.devicehash, dm.token, dm.sound_id, trim(nm.text) AS text, \
+            (case WHEN _from AT TIME ZONE 'UTC' < _to AT TIME ZONE 'UTC' THEN ((_from, _to) OVERLAPS (current_time, current_time)) \
+            ELSE (case WHEN _from <= current_time OR _to >= current_time THEN true ELSE false END) END) AS playsound, \
+            dm.paid, extract(epoch from nm.news_date)::BIGINT AS news_date, dm.type, nm.news_id \
+            FROM apns_master dm, news_master nm \
+            WHERE (dm.news_id & nm.news_id = nm.news_id) AND {device_hash_filter} nm.id = $1"
+        );
+        if let Ok(pool) = Pool::<Postgres>::connect("postgres://breaking:qwertY123@db.iavian.net/breaking").await {
+            let stream = sqlx::query_as::<_, PushNotification>(&sql).bind(news_id).fetch(&pool);
+            stream
+                .for_each_concurrent(Some(self.clients.len()), |notification| async move {
+                    if let Ok(notification) = notification {
+                        let mut hasher = DefaultHasher::new();
+                        notification.device_token.hash(&mut hasher);
+                        let hash = hasher.finish();
+                        let index = (hash % self.clients.len() as u64) as usize;
+                        let client = &self.clients[index];
+                        match client.send_notification(&notification).await {
+                            Ok(push_result) => {
+                                if let Some(result) = push_result {
+                                    if result.status_code == 410 {
+                                        if let Ok(apns_id) = result.apns_id {
+                                            warn!("DB-UPDATE-APNS-FAIL: ID={}, Status={}, Error={:?}", apns_id, result.status_code, result.error);
+                                            if let Ok(inner_pool) = Pool::<Postgres>::connect("postgres://breaking:qwertY123@db.iavian.net/breaking").await {
+                                                if let Err(err) = sqlx::query("UPDATE apns_master SET news_id = 0 WHERE id = $1").bind(apns_id).execute(&inner_pool).await {
+                                                    warn!("Failed to update APNS notification: {err}");
+                                                }
+                                                inner_pool.close().await;
+                                            }
+                                        } else {
+                                            warn!("Failed to parse APNS notification: {result:?}");
+                                        }
+                                    } else {
+                                        warn!("APNS notification send error: {result:?}");
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                warn!("Error sending notification: {err}");
+                            }
+                        }
+                    }
+                })
+                .await;
+
+            pool.close().await;
+        }
     }
 }
